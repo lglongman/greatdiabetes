5966b9bbaffbf49f73fd83984c803534a7dff46f
lglongman <longman627@gmail.com>
2024-11-01 03:03:44 +0800

    added input checkbox options

--- script/control.js
+++ script/control.js
@@ -25,7 +25,8 @@ var last_conIO = [];
 var conIOToggle = [];
 
 function controllerLoop() {
-    const gamepad = navigator.getGamepads()[0];    
+    const gamepad = navigator.getGamepads()[0];
+    const controllerEnable = document.getElementById("controllerEnable");
     if (gamepad) {
         // buttons
         for (var i = 0; i < gamepad.buttons.length; i++) {
@@ -35,7 +36,9 @@ function controllerLoop() {
         // console.log(conIOToggle[UP]);
         getToggle(conIO.length);
 
-        readControllerInput();
+        if (controllerEnable.checked) {
+            readControllerInput();
+        }
     }
 
     requestAnimationFrame(controllerLoop);
@@ -56,11 +59,14 @@ function getToggle(num) {
 window.addEventListener("gamepadconnected", (event)=>{
     alert("DS5 Connected");
     console.log("DS5 Connected");
+    const controllerLbl = document.getElementById("controllerLbl");
+    controllerLbl.textContent = "Controller (Connected)";
     controllerLoop();
 });
 window.addEventListener("gamepaddisconnected", (event)=>{
     alert("DS5 Disonnected");
     console.log("DS5 Disonnected");
+    controllerLbl.textContent = "Controller (Disconnected)";
     cancelAnimationFrame(controllerLoop);
 });
 
@@ -115,58 +121,61 @@ function readControllerInput() {
 
 /*KEYBOARD EVENT LISTENER*/
 window.addEventListener('keydown', function(event) {
-    if (event.key === ' ' && !startBtn.disabled) {
-        event.preventDefault();
-        handleTimer();
-    }
-    else if (event.key === 'Delete' && !resetBtn.disabled) {
-        event.preventDefault();
-        resetButton();
-    }
-    else if (event.key === 'Tab' && !switchBtn.disabled) {
-        event.preventDefault();
-        switchTimerMode();
-    }
-    else if (event.key === 'Escape') {
-        event.preventDefault();
-        showOverlay(false, 0, false);
-    }
-
-    var type = event.shiftKey ? MINUS : ADD;
+    const keyboardEnable = document.getElementById("keyboardEnable");
+    if (keyboardEnable.checked) {
+        if (event.key === ' ' && !startBtn.disabled) {
+            event.preventDefault();
+            handleTimer();
+        }
+        else if (event.key === 'Delete' && !resetBtn.disabled) {
+            event.preventDefault();
+            resetButton();
+        }
+        else if (event.key === 'Tab' && !switchBtn.disabled) {
+            event.preventDefault();
+            switchTimerMode();
+        }
+        else if (event.key === 'Escape') {
+            event.preventDefault();
+            showOverlay(false, 0, false);
+        }
     
-    // RED TEAM
-    if ((event.key === 'q' || event.key === 'Q') && !getButton(RED_TEAM, 1, type).disabled) {
-        event.preventDefault();
-        updateScore(1, RED_TEAM, type == ADD);
-    }
-    else if ((event.key === 'a' || event.key === 'A') && !getButton(RED_TEAM, 2, type).disabled) {
-        event.preventDefault();
-        updateScore(2, RED_TEAM, type == ADD);
-    }
-    else if ((event.key === 'z' || event.key === 'Z') && !getButton(RED_TEAM, 3, type).disabled) {
-        event.preventDefault();
-        updateScore(3, RED_TEAM, type == ADD);
-    }
-    else if ((event.key === 'x' || event.key === 'X') && !getButton(RED_TEAM, 3, CANDY).disabled) {
-        event.preventDefault();
-        handleCandy(RED_TEAM);
-    }
-
-    // BLUE TEAM
-    if ((event.key === 'p' || event.key === 'P') && !getButton(BLUE_TEAM, 1, type).disabled) {
-        event.preventDefault();
-        updateScore(1, BLUE_TEAM, !event.shiftKey);
-    }
-    else if ((event.key === 'l' || event.key === 'L') && !getButton(BLUE_TEAM, 2, type).disabled) {
-        event.preventDefault();
-        updateScore(2, BLUE_TEAM, !event.shiftKey);
-    }
-    else if ((event.key === ',' || event.key === '<') && !getButton(BLUE_TEAM, 3, type).disabled) {
-        event.preventDefault();
-        updateScore(3, BLUE_TEAM, !event.shiftKey);
-    }
-    else if ((event.key === 'm' || event.key === 'M') && !getButton(BLUE_TEAM, 3, CANDY).disabled) {
-        event.preventDefault();
-        handleCandy(BLUE_TEAM);
+        var type = event.shiftKey ? MINUS : ADD;
+        
+        // RED TEAM
+        if ((event.key === 'q' || event.key === 'Q') && !getButton(RED_TEAM, 1, type).disabled) {
+            event.preventDefault();
+            updateScore(1, RED_TEAM, type == ADD);
+        }
+        else if ((event.key === 'a' || event.key === 'A') && !getButton(RED_TEAM, 2, type).disabled) {
+            event.preventDefault();
+            updateScore(2, RED_TEAM, type == ADD);
+        }
+        else if ((event.key === 'z' || event.key === 'Z') && !getButton(RED_TEAM, 3, type).disabled) {
+            event.preventDefault();
+            updateScore(3, RED_TEAM, type == ADD);
+        }
+        else if ((event.key === 'x' || event.key === 'X') && !getButton(RED_TEAM, 3, CANDY).disabled) {
+            event.preventDefault();
+            handleCandy(RED_TEAM);
+        }
+    
+        // BLUE TEAM
+        if ((event.key === 'p' || event.key === 'P') && !getButton(BLUE_TEAM, 1, type).disabled) {
+            event.preventDefault();
+            updateScore(1, BLUE_TEAM, !event.shiftKey);
+        }
+        else if ((event.key === 'l' || event.key === 'L') && !getButton(BLUE_TEAM, 2, type).disabled) {
+            event.preventDefault();
+            updateScore(2, BLUE_TEAM, !event.shiftKey);
+        }
+        else if ((event.key === ',' || event.key === '<') && !getButton(BLUE_TEAM, 3, type).disabled) {
+            event.preventDefault();
+            updateScore(3, BLUE_TEAM, !event.shiftKey);
+        }
+        else if ((event.key === 'm' || event.key === 'M') && !getButton(BLUE_TEAM, 3, CANDY).disabled) {
+            event.preventDefault();
+            handleCandy(BLUE_TEAM);
+        }
     }
 });
